c54e8f40c33812fe79788279c1b0f676ac04abca
Prajwal Hegde <pghegde08@gmail.com>
2025-02-20 21:58:55 +0530

    Zaproxy Framework success

--- SecureScanBackend/src/api/SecureScan.route.js
+++ SecureScanBackend/src/api/SecureScan.route.js
@@ -1,6 +1,5 @@
 import express from "express";
 import secureScanCtrl from "./secureScan.controller.js";
-import zapClientCtrl from "../ZAP Client/zapClient.Controller.js";
 
 const router = express.Router();
 
--- SecureScanBackend/src/api/secureScan.controller.js
+++ SecureScanBackend/src/api/secureScan.controller.js
@@ -7,13 +7,23 @@ export default class secureScanCtrl {
 
     static async apiGetZapSpider(req, res, next) {
         let params = {
+            contextid: 1,
+            userid: null,
             url: "https://public-firing-range.appspot.com/",
+            maxchildren: 10,
             recurse: true,
             inScopeOnly: false
         };
         
-        let spider = zapClientCtrl.getZapSpider(params);
-
+        let spider = await zapClientCtrl.getZapSpider(params);
+        console.log(spider);
         res.json(JSON.stringify(spider));
+    }
+
+    // Update your functionality by creating new functions.
+    static async apiGetSqlInjection(req, res, next) {
+        let params = {
+
+        }
     }
 }
